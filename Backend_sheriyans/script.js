b81b1830355371a522c6e70e6fcca0d6d70d839a
Om Pandey <ompandey628782@gmail.com>
2026-01-21 20:27:33 +0530

    foreach-loop

--- Backend_sheriyans/script.js
+++ Backend_sheriyans/script.js
@@ -1,7 +1,11 @@
-const emails = ["test@gmail.com", "", "admin@gmail.com"];
+// const emails = ["test@gmail.com", "", "admin@gmail.com"];
 
-emails.forEach(email => {
-  if (!email) {
-    console.log("Invalid email  this is not exist");
-  }
-});
+// emails.forEach(email => {
+//   if (!email) {
+//     console.log("Invalid email  this is not exist");
+//   }
+// });
+
+
+const result = [1, 2, 3].forEach(num => num * 2);
+console.log(result); // undefined
